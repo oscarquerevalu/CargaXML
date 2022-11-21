37612d8d504d6b96d8b1576a3d22edc18e2b4836
Oscar Querevalu <oscarquerevalu@gmail.com>
2022-11-21 15:04:59 -0500

    merge

--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -20,8 +20,12 @@ public class Util {
         try (InputStream inputStream = new FileInputStream("consulta.properties")) {
             prop.load(inputStream);
             String cadena_conexion=prop.getProperty("cadena_conexion");
+            String ftp_ipServer=prop.getProperty("ftp_ipServer");
+            String ftp_user=prop.getProperty("ftp_user");
+            String ftp_pass=prop.getProperty("ftp_pass");
+
             System.out.println(cadena_conexion);
-            registro=new Propiedades(cadena_conexion);
+            registro=new Propiedades(cadena_conexion, ftp_ipServer, ftp_user, ftp_pass);
         } catch (IOException io) {
             io.printStackTrace();
         }
@@ -32,6 +36,9 @@ public class Util {
         Properties prop = new Properties();
         try (OutputStream output = new FileOutputStream("consulta.properties")) {
             prop.setProperty("cadena_conexion", registro.getCadena_conexion());
+            prop.setProperty("ftp_ipServer", registro.getFtp_ipServer());
+            prop.setProperty("ftp_user", registro.getFtp_user());
+            prop.setProperty("ftp_pass", registro.getFtp_pass());
             prop.store(output, null);
         } catch (IOException io) {
             io.printStackTrace();
